c0a689793e139b998d3f9241464c3dfe7bdc7cfe
fzerorubigd <fzerorubigd@gmail.com>
2019-11-08 20:21:49 +0100

    add initial tests for hash

---
+++ hash_test.go
@@ -0,0 +1,59 @@
+package bitacoin
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func TestGenerateMask(t *testing.T) {
+	data := []struct {
+		In  int
+		Out []byte
+	}{
+		{
+			In:  0,
+			Out: []byte{},
+		},
+		{
+			In:  1,
+			Out: []byte{0xf},
+		},
+		{
+			In:  2,
+			Out: []byte{0},
+		},
+		{
+			In:  3,
+			Out: []byte{0, 0xf},
+		},
+	}
+
+	for i, d := range data {
+		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
+			t.Parallel()
+			out := GenerateMask(d.In)
+			if !bytes.Equal(out, d.Out) {
+				t.Errorf("Failed for %d, it should be %x but is %x", d.In, d.Out, out)
+			}
+		})
+	}
+}
+
+func ExampleGenerateMask() {
+	mask := GenerateMask(3)
+	fmt.Printf("%x", mask)
+
+	// Output:
+	// 000f
+}
+
+func BenchmarkDifficultHash(b *testing.B) {
+	mask := GenerateMask(3)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		DifficultHash(mask, "a", "b", i)
+	}
+
+	b.ReportAllocs()
+}
