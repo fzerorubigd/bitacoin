159af04a2d53577f69f9878c4c5f8af629f05278
Ruhollah Ahmadi <roohix.code@gmail.com>
2021-09-02 23:38:49 +0430

    Run by two way

--- cmd/bc/bc.go
+++ cmd/bc/bc.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"github.com/fzerorubigd/bitacoin"
 	"log"
+	"os"
 )
 
 const (
@@ -11,16 +12,24 @@ const (
 )
 
 func main() {
-	//var store string
-	//flag.StringVar(&store, "store", os.Getenv("BC_STORE"), "The store to use")
+
 	flag.Usage = usage
 	flag.Parse()
 
-	//s := bitacoin.NewFolderStore(store)
-	//
-	//if err := dispatch(s, flag.Args()...); err != nil {
-	//	log.Fatal(err.Error())
-	//}
+	runBoltDB()
+}
+
+func runByFile() {
+	var store string
+	flag.StringVar(&store, "store", os.Getenv("BC_STORE"), "The store to use")
+	s := bitacoin.NewFolderStore(store)
+
+	if err := dispatch(s, flag.Args()...); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func runBoltDB() {
 
 	d := bitacoin.NewDBStore()
 	if err := dispatch(d, flag.Args()...); err != nil {
